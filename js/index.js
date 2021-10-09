c2f91a03dece3ff8d8504bfaebfddfd97b01bc0d
Francis Githae <francis.githae@student.moringaschool.com>
2021-10-09 21:30:01 +0300

    makethe nav bar sticky

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // This file contains the javascript codes for the website
+// execute the sticky function when a user scrolls
+window.onscroll = function () {
+  makeSticky();
+};
+
+var nav = document.getElementById("nav");
+
+
+// Get the position of the navbar
+var stickyNav = nav.offsetTop;
+
+// Add a sticky class to the navbar on scrolling
+
+function makeSticky() {
+  if (window.pageYOffset > stickyNav) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+
+
