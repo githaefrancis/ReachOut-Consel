afc7e0cee536ba5dfe2b7a9f96677443845c917b
Francis Githae <francis.githae@student.moringaschool.com>
2021-10-11 16:36:50 +0300

    update css rules for links on the content page

--- js/content.js
+++ js/content.js
@@ -1,11 +1,25 @@
 // initialize the returned content -result
-
+var content;
+if (content==null){
 var content="We stand against suicide"
+}
+
+
+//select content to display when user navigates to the content page throught the homepage
+function selectContentReferred(id){
+if(id==="three"){
+  console.log("clicked on loss");
+  content="loss"
+}
+
+
+}
 
 // function to change the content to display on the iframe
 
 function displayContent(id) {
   console.log(id);
+  // display content based on the clicked button
   if (id === "suicidal_ideation") {
     content="We stand against Suicide";
     
@@ -14,6 +28,22 @@ function displayContent(id) {
   else if(id==="addiction"){
     content="We stand against addiction";
   }
+  else if(id==="loss"){
+    content="We stand against loss";
+  }
+  else if(id==="anxiety"){
+    content="We stand against anxiety";
+  }
+  else if(id==="relationship"){
+    content="We stand against relationship";
+  }
+  else if(id==="depression"){
+    content="We stand against depression";
+  }
+  else{
+
+    content="something went wrong,please reload the page";
+  }
 };
 
 //function to set the content on the iframe
@@ -21,3 +51,4 @@ function defaultIframe(){
   document.getElementById("display_iframe").contentDocument.getElementById("category_content").innerHTML=content;
 
 }
+
